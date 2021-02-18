b56450133e307275525355a7f284ef431ce01cca
Samiur-UTI <allpraisethelord@gmail.com>
2021-02-18 19:26:00 +0600

    Bismillah

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const { selection } = require("scenegraph");
-const uxp = require("uxp");
-const fs = uxp.storage.localFileSystem;
 let panel;
 
 function create() {
@@ -25,17 +23,8 @@ function create() {
                 display: none;
             }
         </style>
-        <form method="post" id="main">
-            <div class="row break">
-                <label class="row">
-                    <span>↕︎</span>
-                    <input name="prename" type="text" uxp-quiet="true" id="Prename1" placeholder="Enter Prename" />
-                </label>
-            </div>
-            <footer><button id="ok" type="submit" uxp-variant="cta">Apply</button></footer>
-        </form>
-        <div id="result">
-            <p>Saved Prenames will be displayed here</p>
+        <div id="click">
+            <button id="ok" type="submit" uxp-variant="cta">Get Prenames!</button>
         </div>
         `
     panel = document.createElement("div");
@@ -48,27 +37,29 @@ function show(event) {
     if (!panel) event.node.appendChild(create());
 }
 function addPrenames (){
-    let form = document.querySelector("form");
-    let preName = String(document.querySelector("#Prename1").value);
-    let results = document.querySelector("#result");
-        function showPrenames(){
-            let Html = `<button id="one" type="submit" uxp-variant="cta">${preName}</button>`;
-            results.innerHTML = Html;
-            let button = document.getElementById("one");
-            button.addEventListener("click", () => console.log("Lalala!"))
-            return results;
-        }
-        function sendPrenameData(){
+    let data;
+    let button = document.querySelector("button");
+    // let form = document.querySelector("form");
+    // let preName = String(document.querySelector("#Prename1").value);
+    
+    // let results = document.querySelector("#result");
+    //     function showPrenames(){
+    //         let Html = `<button id="one" type="submit" uxp-variant="cta">${preName}</button>`;
+    //         results.innerHTML = Html;
+    //         let button = document.getElementById("one");
+    //         button.addEventListener("click", () => console.log("Lalala!"))
+    //         return results;
+    //     }
+        function getPrenameData(){
             return new Promise((resolve, reject) => {
-                //var formElement = document.querySelector("form");
-                //let data = new FormData(formElement);
                 let req = new XMLHttpRequest();
                 req.onload = () => {
                     if (req.status === 200) {
                         try {
                             const arr = JSON.parse(req.response);
                             resolve(arr);
-                            console.log(arr)
+                            data = arr;
+                            //console.log(arr)
                         } catch (err) {
                             reject('Couldnt parse response. ${err.message}, ${req.response}');
                         }
@@ -80,11 +71,12 @@ function addPrenames (){
                 req.onabort = reject;
                 req.open("GET", "http://localhost:5000/prename", true);
                 req.send();
+                console.log(data);
                 //console.log((data));
             });
         }
-    form.addEventListener("submit",showPrenames);
-    form.addEventListener("submit", sendPrenameData);
+    button.addEventListener("click", getPrenameData);
+    
 }
 function update(){
     addPrenames();
